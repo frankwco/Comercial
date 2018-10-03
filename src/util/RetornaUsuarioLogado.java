52a279c38d016b643445dc4d5b84c66c0bacc9fd
Frank <frankwco@gmail.com>
2018-10-02 21:52:23 -0300

    Aula 02/10/2018

--- src/util/RetornaUsuarioLogado.java
+++ src/util/RetornaUsuarioLogado.java
@@ -1,20 +1,36 @@
 package util;
 
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import banco.DAOGenerico;
 import modelo.Funcionario;
 
 public class RetornaUsuarioLogado {
-	
+
 	public static Funcionario get() {
-		DAOGenerico<Funcionario> dao = 
-				new DAOGenerico<>(Funcionario.class);
+		DAOGenerico<Funcionario> dao = new DAOGenerico<>(Funcionario.class);
 		Funcionario func = new Funcionario();
-		String nomeUsuario=SecurityContextHolder.getContext()
-				.getAuthentication().getName();
-		func = dao.buscarCondicao("email='"+nomeUsuario+"'")
-							.get(0);
+		String nomeUsuario="";
+		
+		Authentication authentication = (Authentication) 
+				SecurityContextHolder.getContext().getAuthentication(); 
+		if(authentication != null){
+			Object obj = authentication.getPrincipal();
+			if (obj instanceof UserDetails) {
+				nomeUsuario = ((UserDetails)obj).getUsername();
+			} else {
+				nomeUsuario = obj.toString();
+			}		
+		}
+		
+	List<Funcionario> lf = dao.buscarCondicao("email='" + nomeUsuario + "'");
+		if(lf.size()>0) {
+			func = lf.get(0);
+		}
 		return func;
 	}
 
